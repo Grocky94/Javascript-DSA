9734de2425ad56393a8619f3c025f7a0ded4ca5a
Grocky94 <123794431+Grocky94@users.noreply.github.com>
2023-02-19 11:01:29 +0530

    practise

---
+++ TSA.js
@@ -0,0 +1,49 @@
+// // // Q4. print the numbaer from 20-40, which are even?
+// // for( var i=20; i<=40; i++){
+// //     if (i%2===0){
+// //         console.log(i)
+// //     }
+// // }
+
+// // // Q5.print the count from 20-40, which are even ?
+// // var count = 0;
+// // for (var i =20; i<=40;i++){
+// //     if(i%2===0){
+// //         count++;
+// //     }
+// // }
+// // console.log(count);
+
+// // Q6. print count of number from 147-254, which are divisible by 3 and odd?
+// var count= 0;
+// for (var i=147; i<=254; i++){
+//     if (i%3===1){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// for (var i=147; i<=254; i++){
+//     if (i%3===1){
+//         console.log(i);
+//     }
+// }
+
+// // Q find the addition of 2 number to get the target?
+// var target= 20
+// var rocky= [23,5,6,8,5,8,2,10,15,18,45,53,60]
+// for (i=0; i<rocky.length; i++){ 
+//     for(j=i+1; j< rocky.length-1;j++){
+//         if(rocky[i]+rocky[j]===target){
+//             console.log(rocky[i], rocky[j])
+//         }
+//     }
+// }
+
+// find the number between 254-350 , which are divided by 2 and even ?
+// for(i=254; i<=350;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
